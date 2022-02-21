65ebfa8372cade19c076c2ae034aeb877c049911
rakasaputr45s <100151123+rakasaputr45s@users.noreply.github.com>
2022-02-22 04:20:20 +0700

    Add files via upload

---
+++ Ph_Meter___Thinkspeak/Ph_Meter___Thinkspeak.ino
@@ -0,0 +1,80 @@
+
+
+#include <ESP8266WiFi.h>
+#include "secrets.h"
+#include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
+#define SensorPin A0          // the pH meter Analog output is connected with the Arduino’s Analog
+
+char ssid[] = SECRET_SSID;   // your network SSID (name) 
+char pass[] = SECRET_PASS;   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+WiFiClient  client;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+unsigned long int avgValue;  //Store the average value of the sensor feedback
+float b, pH;
+int buf[10],temp;
+ 
+
+void setup() {
+  Serial.begin(115200);  // Initialize serial
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo native USB port only
+  }
+  
+  WiFi.mode(WIFI_STA); 
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
+}
+
+void loop() {
+
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected.");
+  }
+    for(int i=0;i<10;i++)       //Get 10 sample value from the sensor for smooth the value
+  { 
+    buf[i]=analogRead(SensorPin);
+    delay(10);
+  }
+  for(int i=0;i<9;i++)        //sort the analog from small to large
+  {
+    for(int j=i+1;j<10;j++)
+    {
+      if(buf[i]>buf[j])
+      {
+        temp=buf[i];
+        buf[i]=buf[j];
+        buf[j]=temp;
+      }
+    }
+  }
+  avgValue=0;
+  for(int i=2;i<8;i++)                      //take the average value of 6 center sample
+  avgValue+=buf[i];
+  X=(float)avgValue*5.0/1024/6; //convert the analog into millivolt
+  pH = -7.703*X + 41.52;                     //convert the millivolt into pH value
+  Serial.print("pH:");  
+  Serial.print(pH,2);
+  Serial.print(" V:");  
+  Serial.print(X,2);
+  Serial.println(" "); 
+  // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
+  // pieces of information in a channel.  Here, we write to field 1.
+  int x = ThingSpeak.writeField(myChannelNumber, 1, pH, myWriteAPIKey);
+  if(x == 200){
+    Serial.println("Channel update successful.");
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+  delay(20000); // Wait 5 seconds to update the channel again
+}
---
+++ Ph_Meter___Thinkspeak/secrets.h
@@ -0,0 +1,11 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Tunas Pontianak"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "1234567890"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1654358			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "NVQIUH0YOB4ZFHU4"   // replace XYZ with your channel write API Key
+
+// Ph sensor calibration
+float X, cal = -7.500*X + 40.38; //Kalibrasi Variable
